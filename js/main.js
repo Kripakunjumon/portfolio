3d5b4d0aa89c4fa89a2c7bbe8a592d74ceaf07c3
kripa kunjumon <kripakunjumon999@gmail.com>
2023-06-14 00:14:03 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// header scroll
+
+let nav= document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+//nav hide
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
+
+//form
+
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+const forms = document.querySelectorAll('.needs-validation')
+
+// Loop over them and prevent submission
+Array.from(forms).forEach(form => {
+  form.addEventListener('submit', event => {
+    if (!form.checkValidity()) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    form.classList.add('was-validated')
+  }, false)
+})
